7e58619ab5a45c14597207edeec2dfbecfd1220c
githubpat99 <patrick.inauen@gmail.com>
2021-09-21 15:13:38 +0200

    my 1st commit to github

---
+++ app/src/main/java/com/example/myroomexample1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myroomexample1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
